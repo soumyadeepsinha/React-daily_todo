34f7718648e5e36e8bf35baad65c74ef7416efa1
soumyadepsinha <s.sinha2701@gmail.com>
2020-09-12 20:06:52 +0530

    update todo

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,10 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 function TodoForm(props) {
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState(props.edit ? props.edit.value : '')
+
+  const formRef = useRef(null)
+
+  useEffect(() => {
+    formRef.current.focus()
+  })
 
   const handleChange = (e) => {
-    setInput(e.target.value);
+    setInput(e.target.value)
   };
 
   const handleSubmit = (e) => {
@@ -20,17 +26,35 @@ function TodoForm(props) {
   return (
     <React.Fragment>
       <form className="todo_form" onSubmit={handleSubmit}>
-        <input
-          className='todo_input'
-          name='text'
-          value={input}
-          onChange={handleChange}
-          // ref={inputRef}
-          placeholder='Add a todo'
-        />
-        <button onClick={handleSubmit} className='todo-button'>
-          Add todo
-          </button>
+        {props.edit ? (
+          <React.Fragment>
+            <input
+              className='todo_input edit'
+              name='text'
+              value={input}
+              onChange={handleChange}
+              ref={formRef}
+              placeholder='Update your todo'
+            />
+            <button onClick={handleSubmit} className='todo-button edit'>
+              Update todo
+            </button>
+          </React.Fragment>
+        ) : (
+            <React.Fragment>
+              <input
+                className='todo_input'
+                name='text'
+                value={input}
+                onChange={handleChange}
+                ref={formRef}
+                placeholder='Add a todo'
+              />
+              <button onClick={handleSubmit} className='todo-button'>
+                Add new todo
+              </button>
+            </React.Fragment>
+          )}
       </form>
     </React.Fragment>
   );
